111e4f10673a095c6971edc78ba0b2b578493f9a
Jose <jose.zunigahenriquez@gmail.com>
2022-08-04 12:10:10 -0400

    ✨ some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+
+  import React, { useState } from 'react';
+  import { Form, Input, SubmitButton } from 'formik-antd'
+  import { Formik } from 'formik'
+  import * as Yup from 'yup';
+  import './App.css';
+  import { AntDesignOutlined } from "@ant-design/icons"
 
 function App() {
+    
+  const SignupSchema = Yup.object().shape({
+    discordId: Yup.string()
+      .required('Requerido'),
+    email: Yup.string().email('Invalid email').required('Required'),
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  function submit(values) {
+    setLoading(true);
+    setTimeout(() => {
+      console.log(values);
+      setLoading(false);
+    }, 3000);
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      initialValues={{
+        discordId: '',
+        email: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={submit}
+      >
+        {({ errors, touched }) => (
+         <Form 
+         name="basic"
+         labelCol={{ span: 8 }}
+         wrapperCol={{ span: 16 }}
+       >
+         {/* every formik-antd component must have the 'name' prop set: */}
+         <Form.Item name='discordId'>
+           <Input name='discordId' placeholder='Discord Id' />
+          </Form.Item>
+          <Form.Item name='email'>
+            <Input name='email' placeholder='Email' suffix={<span />}/>
+          </Form.Item>
+          <div className='ant-form-item'>
+            <SubmitButton
+              justify="end"
+              style={{ marginTop: 0 }}
+              icon={<AntDesignOutlined />}
+              loading={loading}
+            >
+              Submit
+            </SubmitButton>
+          </div>
+        </Form>
+       )}
+      </Formik>
+
   );
 }
 
